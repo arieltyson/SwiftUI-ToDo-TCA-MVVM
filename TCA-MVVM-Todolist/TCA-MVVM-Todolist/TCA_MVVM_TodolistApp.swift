b8825f7395e8ae80876756914a0718a70987a784
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-13 00:31:01 -0700

    initial commit

---
+++ TCA-MVVM-Todolist/TCA-MVVM-Todolist/TCA_MVVM_TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_MVVM_TodolistApp.swift
+//  TCA-MVVM-Todolist
+//
+//  Created by Ariel Tyson on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_MVVM_TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
