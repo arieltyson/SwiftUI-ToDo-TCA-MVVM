1122bc9e480beb1903044feb34530d523b82ec42
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-16 13:29:54 -0700

    Update app description

--- TCA-MVVM-Todolist/TCA-MVVM-Todolist/TCA_MVVM_TodolistApp.swift
+++ TCA-MVVM-Todolist/TCA-MVVM-Todolist/TCA_MVVM_TodolistApp.swift
@@ -5,6 +5,15 @@
 //  Created by Ariel Tyson on 12/5/25.
 //
 
+/*
+Simple ToDo App demonstrating The Composable Architecture (TCA) + SwiftUI + MVVM + Clean Architecture + Model-Driven Development
+
+Layers:
+- Domain: Task entity, UseCases
+- Data: InMemory repository
+- Presentation: TCA Feature (State, Action, Environment, Reducer), SwiftUI Views
+*/
+
 import SwiftUI
 
 @main
