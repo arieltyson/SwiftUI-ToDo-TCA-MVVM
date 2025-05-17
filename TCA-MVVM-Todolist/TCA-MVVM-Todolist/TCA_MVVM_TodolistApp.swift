36ff2e72d2c6db548654efc5faef1b5ca181fbdc
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-16 23:29:06 -0700

    Create store and UI

--- TCA-MVVM-Todolist/TCA-MVVM-Todolist/TCA_MVVM_TodolistApp.swift
+++ TCA-MVVM-Todolist/TCA-MVVM-Todolist/TCA_MVVM_TodolistApp.swift
@@ -15,12 +15,57 @@ Layers:
 */
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCA_MVVM_TodolistApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let repository = InMemoryTaskRepository()
+            let useCase = TaskUseCase(repository: repository)
+            let store = Store(
+                initialState: TaskFeature.State(),
+                reducer: TaskFeature.reducer,
+                environment: TaskFeature.Environment(
+                    mainQueue: DispatchQueue.main.eraseToAnyScheduler(),
+                    useCase: useCase
+                )
+            )
+            TaskListView(store: store)
+        }
+    }
+}
+
+struct TaskListView: View {
+    let store: Store<TaskFeature.State, TaskFeature.Action>
+
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            NavigationView {
+                VStack {
+                    HStack {
+                        TextField("New Task", text: viewStore.binding(
+                            get: \ .newTitle,
+                            send: TaskFeature.Action.setNewTitle
+                        ))
+                        Button("Add") { viewStore.send(.addButtonTapped) }
+                    }.padding()
+
+                    List {
+                        ForEach(viewStore.tasks) { task in
+                            HStack {
+                                Text(task.title)
+                                Spacer()
+                                Button(action: { viewStore.send(.toggleComplete(id: task.id)) }) {
+                                    Image(systemName: task.isComplete ? "checkmark.circle.fill" : "circle")
+                                }
+                            }
+                        }
+                    }
+                }
+                .navigationTitle("ToDo Demo")
+                .onAppear { viewStore.send(.onAppear) }
+            }
         }
     }
 }
